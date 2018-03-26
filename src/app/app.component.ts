9d9629237ae791fe2f1e8d4ad5ecd7c8ab104bed
Guest User <Guest@epicodus-6.local>
2018-03-26 16:38:23 -0700

    Update styling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "Drew's Brews";
+
+
+
+  kegs: Keg[] = [
+    new Keg("Wud Light", "WudBeiser", 3, 5),
+    new Keg("Olly's Old-Fashioned Lager", "Awful Olly's Brewery", 5, 6.5),
+    new Keg("Shmeineken", "Shmeineken Brewing Co.", 4.50, 5.5),
+    new Keg("Positively Putrid Porter", "Ted's Garage Brewery", 7, 8.6),
+    new Keg("Newe English 40oz", "40's 4 Days", 2, 9)
+  ]
 }
